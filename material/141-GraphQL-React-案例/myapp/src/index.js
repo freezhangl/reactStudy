8b2d2e1866c84b6e12edf763409fc982b3673cc7
zhanglong <zl1050762931@gmail.com>
2023-10-05 17:18:09 +0800

    feat: 添加项目信息

--- material/141-GraphQL-React-案例/myapp/src/index.js
+++ material/141-GraphQL-React-案例/myapp/src/index.js
@@ -5,9 +5,10 @@
 // console.log("hello world kerwin")
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css' //pc的样式
 
 // import App from './15-graphql/App'
-import App from './05-redux/App'
+import App from './07-antd/03-layout'
 
 // import {Provider}  from 'mobx-react'
 // import store from './10-mobx/04-router/mobx/store'
